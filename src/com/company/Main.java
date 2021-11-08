c4d5eae6dfdfd2fa35257bfd25152507d4ba6a0a
Gigotaur <90615070+Gigotaur@users.noreply.github.com>
2021-11-08 17:48:30 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,28 +3,27 @@ package com.company;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.ROOT);
-        double x= enter("Введите вещественное число: ");
-        double n = enter("Введите натуральное число: ");
+        double x = readNum("Введите вещественное число: ");
+        double n = readNum("Введите натуральное число: ");
 
         if (n > 0) {
             double ans = calculateAnswer(x, n);
 
-            if (ans == Double.NEGATIVE_INFINITY || ans == Double.POSITIVE_INFINITY) {
+            if (checkNum(ans)) {
                 System.out.printf("Знаменатель дроби равен нулю.%n");
             } else {
                 System.out.printf("Ответом выражения является %.4f%n", ans);
             }
-        }
-        else {
+        } else {
             System.out.println("Значение n введено некорректно. Пожалуйста, повторите попытку");
         }
     }
 
-    private static double enter(String text){
+    private static double readNum(String text) {
         System.out.println(text);
         Scanner input = new Scanner(System.in);
         double num;
@@ -37,13 +36,16 @@ public class Main{
         double numinator = 1;
         double denuminator = 1;
 
-        for (int i = 1; i <= n; i++)
-        {
+        for (int i = 1; i <= n; i++) {
             numinator *= (x - (2 * i));
             denuminator *= (x - (2 * i - 1));
         }
         ans = numinator / denuminator;
 
         return ans;
+    }
+
+    private static boolean checkNum(double ans) {
+        return ((ans == Double.NEGATIVE_INFINITY || ans == Double.POSITIVE_INFINITY));
     }
 }
